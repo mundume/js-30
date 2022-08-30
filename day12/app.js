d617ccf9a098c2a5cf32381e3c1e9107b827c00a
nzai <nzaih18@gmail.com>
2022-08-30 16:36:56 +0300

    new

--- day12/app.js
+++ day12/app.js
@@ -7,21 +7,29 @@
 //     qtn.classList.toggle('show-text')
 // }))
 
-
+ 
 
 
 // using selector 
 
 const questions = document.querySelectorAll('.question')
-questions.forEach(function(question){
-const btn = question.querySelector(".question-btn")
-console.log(btn)
-btn.addEventListener('click', function(){
-    questions.forEach(function(item){
-    if (item!==question){
-        item.classList.remove('show-text')
-    }
+
+questions.forEach(question=>{
+
+
+    const btn = question.querySelector('.question-btn')
+    btn.addEventListener('click', ()=>{
+
+        questions.forEach(item=>{
+            if(item!==question){
+                item.classList.remove('show-text')
+            }
+        })
+        question.classList.toggle('show-text')
     })
-    question.classList.toggle('show-text')
-})
-})
+    
+        })
+
+       
+     
+   
